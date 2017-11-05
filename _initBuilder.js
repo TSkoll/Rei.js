8425489176e64e93d9a7f6583840b211c0a434dd
DoubleKilled <ronkainen.eero@gmail.com>
2017-11-05 18:40:50 +0200

    Added initBuilder

---
+++ _initBuilder.js
@@ -0,0 +1,8 @@
+/*
+    This file should require all classes that are used in the index.js file and
+    have constructors.
+*/
+
+module.exports = {
+    cmdHandler = require('./cmdHandler/cmdHandler.js')
+};
