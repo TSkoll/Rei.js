6c54ba6c21e8e5eb58798f014bd8b5b06233a82a
DoubleKilled <ronkainen.eero@gmail.com>
2017-11-06 19:41:25 +0200

    Implemented basic DB utilities

---
+++ utils/dbUtil.js
@@ -0,0 +1,76 @@
+const knex = require('knex')((require('../knexfile.js'))['development']);
+
+class dbUtil {
+    static async migrate() {
+        return new Promise(async (resolve, reject) => {
+            // Make sure we have the latest migrations
+            knex.migrate.latest();
+        });
+    }
+
+    static async addData(table, data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let row = await knex(table).insert(data);
+                resolve(row);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    static async getRow(table, searchPattern) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let data = await knex(table)
+                .select()
+                .where(searchPattern);
+
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+        })
+    }
+
+    static async getSpecificFromRow(table, select, searchPattern) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let data = await knex(table)
+                .select(select)
+                .where(searchPattern);
+
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+        })
+    }
+
+    static async updateRow(table, searchPattern, updatedData) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let row = await knex(table)
+                .where(searchPattern)
+                .update(updatedData);
+
+                resolve(row);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    static async ifRowExists(table, searchPattern) {
+        try {
+            let rows = await knex(table)
+            .select()
+            .where(searchPattern);
+
+            resolve(rows.length);
+        } catch (err) {
+            reject(err);
+        }
+    }
+}
+module.exports = dbUtil;
