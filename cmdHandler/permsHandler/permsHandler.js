3df8c6ce3a907d2da12c9eabf1025ea3dc18deed
DoubleKilled <ronkainen.eero@gmail.com>
2017-12-11 14:53:07 +0200

    Add documentation to permsHandler.js

--- cmdHandler/permsHandler/permsHandler.js
+++ cmdHandler/permsHandler/permsHandler.js
@@ -28,30 +28,35 @@ class permissionHandler {
         });
     }
 
+    // Check if command was run by it's owner
     static async checkOwnerOnly(command, id) {
         return new Promise((resolve, reject) => {
 
         });
     }
-
+    
+    // Check if the bot has enough permissions to run the command successfully
     static async checkBotPerms(command, client) {
         return new Promise((resolve, reject) => {
 
         });
     }
 
+    // Check if the user has enough permissions to run the command successfully
     static async checkUserPerms(command, user) {
         return new Promise((resolve, reject) => {
 
         });
     }
 
+    // Check if the command was run by it's owner
     static async checkGuildOwnership(command, guild, user) {
         return new Promise((resolve, reject) => {
 
         });
     }
 
+    // Check if command was run in a private channel
     static async checkDMChannel(command, channel) {
         return new Promise((resolve, reject) {
 
