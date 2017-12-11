1caa7aeffa2fb44f62680d5146ec5a2579b2581d
DoubleKilled <ronkainen.eero@gmail.com>
2017-12-11 14:45:22 +0200

    Add documentation to argParser.js

--- cmdHandler/argParser.js
+++ cmdHandler/argParser.js
@@ -1,46 +1,69 @@
 class argParser {
+    /* Parses input into a list of arguments
+        handles "quoted messages"*/
     static parse(input, argAmount, ignoreMin) {
         let args = [];
 
         let quote = false;
         let quoteCache = [];
-
+        
+        // We can just return unedited string if argument count is less than 2
         if (argAmount < 2)
             return input;
 
+        // Do a basic split in the beginning
         let split = input.split(' ');
-        for (let i = 0; i < input.length; i++) {
+        
+        // Loop through the list of split strings
+        for (let i = 0; i < split.length; i++) {
             let s = split[i];
-
+            
+            // handle multiple whitespaces
             if (s == null)
                 continue;
-
+            
+            // Check if split string starts with a quote
             if (s[0] == "\"" && quote == false) {
+                // Remove quote and add to a seperate list
                 quoteCache.push(s.substr(1, s.length - 1));
 
+                // Start seeking for another quote
                 quote = true;
                 continue;
             }
 
             if (quote == true) {
+                // Check if string ends with a quote, while seeking for one
                 if (s[s.length - 1] == "\"") {
+                    // Add word to the cache and remove trailing quote
                     quoteCache.push(s.substr(0, s.length - 1));
+                    
+                    // Join words in the cache and push it to returnable list
                     args.push(quoteCache.join(' '));
+                    
+                    // Flush cache
                     quoteCache = [];
 
+                    // Stop seeking for quotes
                     quote = false;
                     continue;
                 } else {
+                    // Add words seperated by whitespace but still inside quotes to cache
                     quoteCache.push(s);
                 }
             } else {
+                // Add non-quoted words straight to returnable list
                 args.push(s);
             }
         }
 
+        // Check if cache contains something and push it to returnable list
+        // Handles cases like '"Hello World' without making the string vanish
         if (quoteCache.length > 0)
             args.push(quoteCache.join(' '));
         
+        // Check if we managed to build enough arguments for the command
+        // Or if the command even needs a set amount in the first place
         if (args.length == argAmount && !ignoreMin)
             throw "Not enough arguments!";
         else
