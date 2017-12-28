72e0e207cdd062e2365a7a5fe239924f8492f489
DoubleKilled <ronkainen.eero@gmail.com>
2017-12-28 23:20:45 +0200

    Add null check to command flag checks

--- Types/command.js
+++ Types/command.js
@@ -3,19 +3,19 @@ const Discord = require('discord.js');
 class Command {
     constructor(info) {
         /* Flags */
-        this.ownerOnly = (info.hasOwnProperty('ownerOnly'))     ? info.ownerOnly    : false;
-        this.args = (info.hasOwnProperty('args'))               ? info.args         : 0;
-        this.ignoreMin = (info.hasOwnProperty('ignoreMin'))     ? info.ignoreMin    : false;
-        this.showOnHelp = (info.hasOwnProperty('showOnHelp'))   ? info.showOnHelp   : true;
-        this.botPerms = (info.hasOwnProperty('botPerms'))       ? info.botPerms     : null;
-        this.userPerms = (info.hasOwnProperty('userPerms'))     ? info.userPerms    : null;
-        this.guildOwner = (info.hasOwnProperty('guildOwner'))   ? info.guildOwner   : false;
-        this.cost = (info.hasOwnProperty('cost'))               ? info.cost         : false;
-        this.disallowDM = (info.hasOwnProperty('disallowDM'))   ? info.disallowDM   : false;
+        this.ownerOnly = (info && info.hasOwnProperty('ownerOnly'))     ? info.ownerOnly    : false;
+        this.args = (info && info.hasOwnProperty('args'))               ? info.args         : 0;
+        this.ignoreMin = (info && info.hasOwnProperty('ignoreMin'))     ? info.ignoreMin    : false;
+        this.showOnHelp = (info && info.hasOwnProperty('showOnHelp'))   ? info.showOnHelp   : true;
+        this.botPerms = (info && info.hasOwnProperty('botPerms'))       ? info.botPerms     : null;
+        this.userPerms = (info && info.hasOwnProperty('userPerms'))     ? info.userPerms    : null;
+        this.guildOwner = (info && info.hasOwnProperty('guildOwner'))   ? info.guildOwner   : false;
+        this.cost = (info && info.hasOwnProperty('cost'))               ? info.cost         : false;
+        this.disallowDM = (info && info.hasOwnProperty('disallowDM'))   ? info.disallowDM   : false;
 
         /* Help information */
-        this.helpText = (info.hasOwnProperty('helpText'))       ? info.helpText     : null;
-        this.helpArgs = (info.hasOwnProperty('helpArgs'))       ? info.helpArgs     : null;
+        this.helpText = (info && info.hasOwnProperty('helpText'))       ? info.helpText     : null;
+        this.helpArgs = (info && info.hasOwnProperty('helpArgs'))       ? info.helpArgs     : null;
     }
 
     /* Functions */
