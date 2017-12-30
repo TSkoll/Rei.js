68b1e01d6b844bea750253e437d950da0f8feb51
DoubleKilled <ronkainen.eero@gmail.com>
2017-12-30 11:32:31 +0200

    Implemented leaveserver

---
+++ modules/Owner/leaveserver.js
@@ -0,0 +1,23 @@
+const Command = require('../../Types/command.js');
+
+class Leaveserver extends Command {
+    constructor() {
+        super({
+            "ownerOnly": true,
+            "args": 1,
+            "ignoreMin": true
+        });
+    }
+
+    async run(bot, msg, args) {
+        if (args.length == 0) {
+            await msg.guild.leave();
+        } else {
+            const guild = await bot.user.guild.find('id', args);
+            
+            await guild.leave();
+            await super.sendBasicSuccess('Left server!');
+        }
+    }
+}
+module.exports = Leaveserver;
