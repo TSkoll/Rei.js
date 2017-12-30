194c7efdaf2cb3855155729a2228bb62330e7820
DoubleKilled <ronkainen.eero@gmail.com>
2017-12-30 11:32:38 +0200

    Implemented sleep

---
+++ modules/Owner/sleep.js
@@ -0,0 +1,18 @@
+const Command = require('../../Types/command.js');
+const Discord = require('discord.js');
+
+class Sleep extends Command {
+    constructor() {
+        super({
+            "ownerOnly": true
+        });
+    }
+
+    async run(bot, msg, args) {
+        await super.sendEmbed(new Discord.RichEmbed()
+        .setColor('DARK_BLUE')
+        .setDescription('Good night!'));
+        process.exit();
+    }
+}
+module.exports = Sleep;
