1f645260d46a07b6793d18d73ad5a357fb4e51f2
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:01:12 +0200

    prettify sendQuote

--- modules/Misc/quote/sendQuote.js
+++ modules/Misc/quote/sendQuote.js
@@ -1,39 +1,42 @@
-const Discord = require('discord.js');
-const { calcTimeDifference } = require('../../../utils/timeUtil.js');
+const Discord = require("discord.js");
+const { calcTimeDifference } = require("../../../utils/timeUtil.js");
 
 module.exports = async (msg, id, channel) => {
     let quoteMsg;
     try {
         quoteMsg = await channel.fetchMessage(id);
     } catch (e) {
-        throw 'I couldn\'t find the message that you\'re looking for';
+        throw "I couldn't find the message that you're looking for";
     }
 
-    if(quoteMsg.type != 'DEFAULT')
-        throw 'You can only quote messages sent by other users!';
+    if (quoteMsg.type != "DEFAULT")
+        throw "You can only quote messages sent by other users!";
 
-    if(channel.nsfw && !msg.channel.nsfw)
-        throw 'You can\'t quote nsfw things here!';
+    if (channel.nsfw && !msg.channel.nsfw)
+        throw "You can't quote nsfw things here!";
 
     /* 
     If quoted message has one or more embeds (within which at least one is a rich embed), 
     send an embed from the quoted message and the content (if that message has one)
     otherwise, send content and attachments in an embed
     */
-    if(quoteMsg.embeds.length > 0 && quoteMsg.embeds.find(e => e.type == 'rich'))
+    if (
+        quoteMsg.embeds.length > 0 &&
+        quoteMsg.embeds.find((e) => e.type == "rich")
+    )
         return quoteWithEmbed(msg, quoteMsg);
-    else
-        return defaultQuote(msg, quoteMsg);
-}
+    else return defaultQuote(msg, quoteMsg);
+};
 
 function quoteWithEmbed(msg, quoteMsg) {
     //Find rich embed in messages' embeds
-    const quoteObj = quoteMsg.embeds.find(e => e.type == 'rich');
-    
+    const quoteObj = quoteMsg.embeds.find((e) => e.type == "rich");
+
     //Declare content string so if message has its own, we can just concatenate them
-    let content = `**${quoteMsg.author.tag}** sent this embed ${diffString(quoteMsg)}`;
-    if(quoteMsg.content)
-        content += `\n\n${quoteMsg.content}`;
+    let content = `**${quoteMsg.author.tag}** sent this embed ${diffString(
+        quoteMsg
+    )}`;
+    if (quoteMsg.content) content += `\n\n${quoteMsg.content}`;
 
     return sendQuote(msg, parseMessageEmbed(quoteObj), content);
 }
@@ -45,59 +48,64 @@ function parseMessageEmbed(quoteObj) {
     Check for each embed property, since they're optional,
     we have to check if an embed has them to not accidentally try to set something to undefined
     */
-    if(quoteObj.description)
-      embed.setDescription(quoteObj.description);
+    if (quoteObj.description) embed.setDescription(quoteObj.description);
 
-    if(quoteObj.author)
-       embed.setAuthor(quoteObj.author.name, quoteObj.author.iconURL, quoteObj.author.url);
+    if (quoteObj.author)
+        embed.setAuthor(
+            quoteObj.author.name,
+            quoteObj.author.iconURL,
+            quoteObj.author.url
+        );
 
-    if(quoteObj.title)
-       embed.setTitle(quoteObj.title);
+    if (quoteObj.title) embed.setTitle(quoteObj.title);
 
-    if(quoteObj.thumbnail)
-       embed.setThumbnail(quoteObj.thumbnail.url);
+    if (quoteObj.thumbnail) embed.setThumbnail(quoteObj.thumbnail.url);
 
-    if(quoteObj.image)
-       embed.setImage(quoteObj.image.url);
+    if (quoteObj.image) embed.setImage(quoteObj.image.url);
 
-    if(quoteObj.createdTimestamp)
-       embed.setTimestamp(quoteObj.createdTimestamp);
+    if (quoteObj.createdTimestamp)
+        embed.setTimestamp(quoteObj.createdTimestamp);
 
-    if(quoteObj.footer)
-       embed.setFooter(quoteObj.footer.text, quoteObj.footer.iconURL);
+    if (quoteObj.footer)
+        embed.setFooter(quoteObj.footer.text, quoteObj.footer.iconURL);
 
     //For each of quote's embed's fields add a new field to the new embed
-    quoteObj.fields.forEach(field => embed.addField(field.name, field.value, field.inline));
+    quoteObj.fields.forEach((field) =>
+        embed.addField(field.name, field.value, field.inline)
+    );
 
     return embed;
 }
 
 function defaultQuote(msg, quoteMsg) {
     const embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
+        .setColor("RANDOM")
         .setAuthor(quoteMsg.author.tag, quoteMsg.author.avatarURL)
         .setDescription(quoteMsg.cleanContent)
         .setFooter(`Sent ${diffString(quoteMsg)}`);
 
     //If quoted message has attachments...
-    if(quoteMsg.attachments.size > 0) {
+    if (quoteMsg.attachments.size > 0) {
         //...attach first of them to the embed
         const attachments = Array.from(quoteMsg.attachments.values());
         embed.setImage(attachments[0].url);
 
         //and if that message has more attachments, just say the total amount of them
-        if(attachments.length > 1)
-            embed.addField('Message contains more attachments than just one.', `A total of ${attachments.length} were found.`);
+        if (attachments.length > 1)
+            embed.addField(
+                "Message contains more attachments than just one.",
+                `A total of ${attachments.length} were found.`
+            );
     }
 
     return sendQuote(msg, embed);
 }
 
-async function sendQuote(msg, embed, content = '') {
+async function sendQuote(msg, embed, content = "") {
     await msg.channel.send(content, { embed });
 }
 
-function diffString (quoteMsg) {
+function diffString(quoteMsg) {
     //Get an object with time differences between now and when the quoted message was sent
     const timeDiff = calcTimeDifference(Date.now() - quoteMsg.createdTimestamp);
 
@@ -105,14 +113,13 @@ function diffString (quoteMsg) {
         //If that time difference is greater than 0
         if (timeDiff[key] > 0) {
             //and greater than one, just return it
-            if(timeDiff[key] > 1)
-                return `${timeDiff[key]} ${key} ago`;
-            //otherwise crop 's' from the end (I hate when something says for example '1 hours' so I had to do it this way)
+            if (timeDiff[key] > 1) return `${timeDiff[key]} ${key} ago`;
             else
+                //otherwise crop 's' from the end (I hate when something says for example '1 hours' so I had to do it this way)
                 return `1 ${key.slice(0, -1)} ago`;
         }
     }
 
     //If somehow someone tries to quote a message that was sent less than a second ago, just return 'now'
-    return 'now';
+    return "now";
 }
