5607193a47e86608b3b371efa8d31e751d7a0a95
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:04:11 +0200

    prettify sleep

--- modules/Owner/sleep.js
+++ modules/Owner/sleep.js
@@ -1,5 +1,5 @@
-const Command = require('../../Types/command.js');
-const Discord = require('discord.js');
+const Command = require("../../Types/command.js");
+const Discord = require("discord.js");
 
 class Sleep extends Command {
     constructor() {
@@ -9,10 +9,13 @@ class Sleep extends Command {
     }
 
     async run(bot, msg, args) {
-        await super.sendEmbed(msg, new Discord.RichEmbed()
-        .setColor('DARK_BLUE')
-        .setDescription('Good night!'));
-        
+        await super.sendEmbed(
+            msg,
+            new Discord.RichEmbed()
+                .setColor("DARK_BLUE")
+                .setDescription("Good night!")
+        );
+
         await bot.destroy();
         process.exit();
     }
