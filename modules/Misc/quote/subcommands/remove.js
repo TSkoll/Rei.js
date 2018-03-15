7d9c5cd88d0d92dd8c407ab30763bf0c034c1d8f
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:00:49 +0200

    prettify remove

--- modules/Misc/quote/subcommands/remove.js
+++ modules/Misc/quote/subcommands/remove.js
@@ -1,16 +1,20 @@
-const db = require('../../../../utils/dbUtil.js');
+const db = require("../../../../utils/dbUtil.js");
 
 module.exports = async function(msg, args) {
-    if (args.length < 2)
-        throw 'Not enough arguments!';
+    if (args.length < 2) throw "Not enough arguments!";
 
-    const rows = await db.getRows('quotes', { 'guildid': msg.guild.id, 'name': args.slice(1).join(' ') });
+    const rows = await db.getRows("quotes", {
+        guildid: msg.guild.id,
+        name: args.slice(1).join(" ")
+    });
 
-    if (rows.length < 1)
-        throw 'That quote doesn\'t seem to exist!';
+    if (rows.length < 1) throw "That quote doesn't seem to exist!";
 
-    if ((rows[0].userid != msg.author.id) && !msg.member.hasPermission('MANAGE_MESSAGES'))
-        throw 'You can\'t remove this quote!';
+    if (
+        rows[0].userid != msg.author.id &&
+        !msg.member.hasPermission("MANAGE_MESSAGES")
+    )
+        throw "You can't remove this quote!";
 
-    await db.deleteRows('quotes', rows[0]);
-}
+    await db.deleteRows("quotes", rows[0]);
+};
