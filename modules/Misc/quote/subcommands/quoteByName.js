a7fd0fbdc146e66e53469c8305fd9e24bc945d5f
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:00:35 +0200

    prettify quoteByName

--- modules/Misc/quote/subcommands/quoteByName.js
+++ modules/Misc/quote/subcommands/quoteByName.js
@@ -1,13 +1,16 @@
-const db = require('../../../../utils/dbUtil.js');
-const sendQuote = require('../sendQuote.js');
+const db = require("../../../../utils/dbUtil.js");
+const sendQuote = require("../sendQuote.js");
 
 module.exports = async function(msg, args) {
-    const rows = await db.getRows('quotes', { 'guildid': msg.guild.id, 'name': args.join(' ') });
+    const rows = await db.getRows("quotes", {
+        guildid: msg.guild.id,
+        name: args.join(" ")
+    });
 
     if (rows.length < 1)
-        throw 'I wasn\'t able to find the quote you were looking for.'
+        throw "I wasn't able to find the quote you were looking for.";
 
     const channel = msg.guild.channels.get(rows[0].channelid);
 
     return sendQuote(msg, rows[0].messageid, channel);
-}
+};
