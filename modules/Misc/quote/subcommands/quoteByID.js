c38e0547998638800395e16bd6b719fec417cd24
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:00:21 +0200

    prettify quoteById

--- modules/Misc/quote/subcommands/quoteByID.js
+++ modules/Misc/quote/subcommands/quoteByID.js
@@ -1,5 +1,5 @@
-const sendQuote = require('../sendQuote.js');
+const sendQuote = require("../sendQuote.js");
 
 module.exports = function(msg, args) {
     return sendQuote(msg, args[0], msg.channel);
-}
+};
