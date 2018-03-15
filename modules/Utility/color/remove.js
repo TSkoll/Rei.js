b5b11f35bacaa5db89e680635f8e2fea6812a42b
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:05:29 +0200

    prettify remove

--- modules/Utility/color/remove.js
+++ modules/Utility/color/remove.js
@@ -1,5 +1,7 @@
 module.exports = async function(msg) {
-    const serverColorRoles = msg.member.guild.roles.filter(x => x.name[0] == '#');
+    const serverColorRoles = msg.member.guild.roles.filter(
+        (x) => x.name[0] == "#"
+    );
 
     await msg.member.removeRoles(serverColorRoles);
-}
+};
