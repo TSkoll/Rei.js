95361c2ab41f8b8d0b993b477a761486141d6ba2
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:01:24 +0200

    prettify list

--- modules/Misc/tag/list.js
+++ modules/Misc/tag/list.js
@@ -1,33 +1,37 @@
-const db = require('../../../utils/dbUtil.js');
-const tagUtils = require('./tagUtils.js');
+const db = require("../../../utils/dbUtil.js");
+const tagUtils = require("./tagUtils.js");
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = async function(msg, args) {
     // Get tags for that user
-    const rows = await db.getRows('tags', { 'userid': msg.author.id });
+    const rows = await db.getRows("tags", { userid: msg.author.id });
 
-    if (rows.length < 1)
-        throw 'You haven\'t saved any tags yet';
+    if (rows.length < 1) throw "You haven't saved any tags yet";
 
     let embed = new Discord.RichEmbed()
-    .setTitle(`You have ${rows.length} saved ${(rows.length == 1) ? 'tag' : 'tags'}`)
-    .setColor('BLUE');
+        .setTitle(
+            `You have ${rows.length} saved ${rows.length == 1 ? "tag" : "tags"}`
+        )
+        .setColor("BLUE");
 
-    let description = '';
+    let description = "";
     for (let i = 0; i < rows.length; i++) {
         // Build the description of tags from found rows
         const row = rows[i];
 
-        let content = '';
+        let content = "";
         if (row.content)
-            content = (row.content.length > 17)
-                ? row.content.substring(0, 17) + '...'
-                : row.content;
-        
-        description += `**${row.name}**: ${content} ${row.imageid ? ':frame_photo:' : ''}\n`;
+            content =
+                row.content.length > 17
+                    ? row.content.substring(0, 17) + "..."
+                    : row.content;
+
+        description += `**${row.name}**: ${content} ${
+            row.imageid ? ":frame_photo:" : ""
+        }\n`;
     }
     embed.setDescription(description);
 
     msg.channel.send(embed);
-}
+};
