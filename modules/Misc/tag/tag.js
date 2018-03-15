3e800c675b9b217f005dc51af9ac75878d803b8d
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:02:01 +0200

    prettify tag

--- modules/Misc/tag/tag.js
+++ modules/Misc/tag/tag.js
@@ -1,23 +1,24 @@
-const db = require('../../../utils/dbUtil.js');
-const tagUtils = require('./tagUtils.js');
+const db = require("../../../utils/dbUtil.js");
+const tagUtils = require("./tagUtils.js");
 
 module.exports = async function(msg, args) {
-    const rows = await db.getRows('tags', { 'userid': msg.author.id, 'name': args.join(' ') });
+    const rows = await db.getRows("tags", {
+        userid: msg.author.id,
+        name: args.join(" ")
+    });
 
-    if (rows.length < 1)
-        throw 'This tag doesn\'t seem to exist!';
+    if (rows.length < 1) throw "This tag doesn't seem to exist!";
 
-    let content = ''
+    let content = "";
     let file = null;
 
-    if (rows[0].content)
-        content = rows[0].content;
+    if (rows[0].content) content = rows[0].content;
 
-    if (rows[0].imageid)
-        file = await tagUtils.loadImage(rows[0].imageid);
-    
+    if (rows[0].imageid) file = await tagUtils.loadImage(rows[0].imageid);
+
     if (file)
-        await msg.channel.send(content, { files: [ { attachment: file, name: rows[0].imageid } ] });
-    else
-        await msg.channel.send(content);
-}
+        await msg.channel.send(content, {
+            files: [{ attachment: file, name: rows[0].imageid }]
+        });
+    else await msg.channel.send(content);
+};
