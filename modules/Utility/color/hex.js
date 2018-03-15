6f4066fc4a124a1c442d5ec3b11fe74383ca3ead
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:05:03 +0200

    prettify hex

--- modules/Utility/color/hex.js
+++ modules/Utility/color/hex.js
@@ -1,19 +1,16 @@
-const validator = require('validator');
+const validator = require("validator");
 
-const assign = require('./assign.js');
+const assign = require("./assign.js");
 
 module.exports = async function(msg, cr) {
-    if (cr[0] != '#')
-        cr = '#' + cr;
+    if (cr[0] != "#") cr = "#" + cr;
 
     cr = cr.toUpperCase();
-    
+
     try {
-        if (validator.isHexColor(cr))
-            await assign(msg, cr);
-        else
-            throw 'That doesn\'t seem to be a valid hex color!';
+        if (validator.isHexColor(cr)) await assign(msg, cr);
+        else throw "That doesn't seem to be a valid hex color!";
     } catch (err) {
         throw { expected: true, message: err };
     }
-}
+};
