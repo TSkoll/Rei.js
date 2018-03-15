64c8d9fbe66a0fdaf3f47670bbc839f8adb77605
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:01:35 +0200

    prettify remove

--- modules/Misc/tag/remove.js
+++ modules/Misc/tag/remove.js
@@ -1,15 +1,16 @@
-const db = require('../../../utils/dbUtil.js');
-const tagUtils = require('./tagUtils.js');
+const db = require("../../../utils/dbUtil.js");
+const tagUtils = require("./tagUtils.js");
 
 module.exports = async function(msg, args) {
     // Get the row user wants to delete
-    const rows = await db.getRows('tags', { 'userid': msg.author.id, 'name': args.slice(1).join(' ') });
-    
-    if (rows.length < 1)
-        throw 'I can\'t delete a tag that doesn\'t exist!';
+    const rows = await db.getRows("tags", {
+        userid: msg.author.id,
+        name: args.slice(1).join(" ")
+    });
+
+    if (rows.length < 1) throw "I can't delete a tag that doesn't exist!";
 
     // Delete row from db and if image exists in tagImages, delete it too.
-    await db.deleteRows('tags', rows[0]);
-    if (rows[0].imageid)
-        await tagUtils.deleteImage(rows[0].imageid);
-}
+    await db.deleteRows("tags", rows[0]);
+    if (rows[0].imageid) await tagUtils.deleteImage(rows[0].imageid);
+};
