544da8da66f4c69d3fc48a651ab1f5b1a7c447e4
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:08:03 +0200

    remove _initBuilder.js

--- _initBuilder.js
+++
@@ -1,8 +0,0 @@
-/*
-    This file should require all classes that are used in the index.js file and
-    have constructors.
-*/
-
-module.exports = {
-    cmdHandler = require('./cmdHandler/cmdHandler.js')
-};
