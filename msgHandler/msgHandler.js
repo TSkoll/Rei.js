6e317e86d76f00ed92befde2f713e0d09e561ce2
DoubleKilled <ronkainen.eero@gmail.com>
2018-03-15 12:06:05 +0200

    prettify msgHandler

--- msgHandler/msgHandler.js
+++ msgHandler/msgHandler.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js');
-const db = require('../utils/dbUtil.js');
-const defaultPrefix = require('../data/config.json').defaultPrefix;
-const cmdHandler = require('../cmdHandler/cmdHandler.js');
+const Discord = require("discord.js");
+const db = require("../utils/dbUtil.js");
+const defaultPrefix = require("../data/config.json").defaultPrefix;
+const cmdHandler = require("../cmdHandler/cmdHandler.js");
 
 class msgHandler {
     constructor(client, cmdPass) {
@@ -15,19 +15,31 @@ class msgHandler {
 
     async onMessageEvent(message) {
         // Skip bot's messages
-        if (message.author.id == this.client.user.id)
-            return;
+        if (message.author.id == this.client.user.id) return;
 
         // Add 1 to messages received counter
         this.statTracker.messageAdd();
 
         // Get guild's command prefix
-        const prefix = (message.guild) ? await this.prefixHandler.get(message.guild.id) : defaultPrefix;
+        const prefix = message.guild
+            ? await this.prefixHandler.get(message.guild.id)
+            : defaultPrefix;
 
         if (message.content.startsWith(prefix)) {
             // Split the command and arguments from each other testprefix set !
-            const cmd = (message.content.indexOf(' ') > 0) ? message.content.substring(prefix.length, message.content.indexOf(' ')) : message.content.substring(prefix.length);
-            const argString = (message.content.indexOf(' ') > 0) ? message.content.substring(message.content.indexOf(' ') + 1) : null;
+            const cmd =
+                message.content.indexOf(" ") > 0
+                    ? message.content.substring(
+                          prefix.length,
+                          message.content.indexOf(" ")
+                      )
+                    : message.content.substring(prefix.length);
+            const argString =
+                message.content.indexOf(" ") > 0
+                    ? message.content.substring(
+                          message.content.indexOf(" ") + 1
+                      )
+                    : null;
 
             try {
                 await this.cmdHandler.run(message, cmd, argString);
@@ -43,9 +55,11 @@ module.exports = msgHandler;
 
 /* Error messaging */
 async function throwErr(message, err) {
-    await message.channel.send(new Discord.RichEmbed()
-    .setColor('YELLOW')
-    .setTitle('Woops!')
-    .setDescription(err)
-    .setFooter('This is an error. You should not be seeing this.'));
+    await message.channel.send(
+        new Discord.RichEmbed()
+            .setColor("YELLOW")
+            .setTitle("Woops!")
+            .setDescription(err)
+            .setFooter("This is an error. You should not be seeing this.")
+    );
 }
