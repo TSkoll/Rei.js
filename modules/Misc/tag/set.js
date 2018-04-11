d5f610dba509b8a6b56c6c93eea4e56fd3463473
DoubleKilled <ronkainen.eero@gmail.com>
2018-04-11 22:37:49 +0300

    woops

--- modules/Misc/tag/set.js
+++ modules/Misc/tag/set.js
@@ -2,13 +2,14 @@ const db = require('../../../utils/dbUtil.js');
 const tagUtils = require('./tagUtils.js');
 
 module.exports = async function(msg, args) {
-    if (args.length < 2)
+    if (args.length < 2 || (args.length == 2 && msg.attachments.size == 0))
         throw 'Not enough arguments!';
 
     if (await db.ifRowExists('tags', { 'userid': msg.author.id, 'name': args[1] }))
         throw 'A tag with that name already exists!';
 
     const img = (msg.attachments.size > 0) ? msg.attachments.first() : null;
+
     await db.addData('tags', {
         'userid': msg.author.id,
         'name': args[1],
