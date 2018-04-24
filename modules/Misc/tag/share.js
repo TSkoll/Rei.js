e9a381b1bd248b5e84770925ada3dd69a4751f29
nrabulinski <nikodemrabulinski@gmail.com>
2018-04-24 17:55:18 +0200

    Major improvements and bug fixes

--- modules/Misc/tag/share.js
+++ modules/Misc/tag/share.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const db = require('../../../utils/dbUtil.js');
 
+const maxMentions = 10;
+
 module.exports = async function(msg, args) {
     if(args.length < 3)
         throw 'Not enough arguments!';
@@ -9,53 +11,54 @@ module.exports = async function(msg, args) {
     if(!tag)
         throw 'I can\'t share a tag that doesn\'t exist!';
 
-    if(msg.mentions.users.size == 0)
-        throw 'You have to mention users to share this tag to!';
-
     // Get all of the mentions from the message
-    const awaitingUsers = Array.from(msg.mentions.users.values());
+    const awaitingUsers = [];
+    Array.from(msg.mentions.users.values()).filter(u => !u.bot && u != msg.author).forEach(u => {
+        if(!awaitingUsers.includes(u) && awaitingUsers.length < maxMentions)
+            awaitingUsers.push(u);
+    });
+
+    if(awaitingUsers.length == 0)
+        throw 'You have to mention users to share this tag to!';
 
     // Generate random code so users can confirm sharing
     const code = Math.random().toString(36).substr(2, 6);
     
-    for(const user of awaitingUsers) {
-        // Can't share the tag with yourself and bot accounts
-        if(user.bot || user == msg.author) {
-            awaitingUsers.splice(
-                awaitingUsers.indexOf(user.id), 1
-            );
-            continue;
-        }
+    const msgC = new Discord.MessageCollector(
+        msg.channel, 
+        m => m.content.startsWith(code) && awaitingUsers.includes(m.author), 
+        { time: 300000 }
+    );
 
-        const msgC = new Discord.MessageCollector(msg.channel, a => a.author == user, { time: 300000 });
-        msgC.on('collect', async message => {
-            if(!message.content.startsWith(code))
-                return;
+    msgC.on('collect', async m => {
+        // Split message by spaces, remove the code and concat it together again
+        const newName = m.content.split(' ').slice(1).filter(a => a != '').join(' ');
 
-            // Split message by spaces, remove the code and concat it together again
-            const newName = message.content.split(' ').slice(1).join(' ');
+        if(await db.ifRowExists('tags', { 'userid': m.author.id, 'name': tag.name }) && !newName)
+            return await msg.channel.send(`You have to provide a new name since you already have a tag with the same name`);
 
-            if(await db.ifRowExists('tags', { 'userid': user.id, 'name': tag.name }) && !newName)
-                return await msg.channel.send(`${user}, you have to provide a new name since you already have a tag with the same name`);
+        // Fancy way for merging objects together
+        const newTag = Object.assign({}, tag, { 'name': newName ? newName : tag.name, 'userid': m.author.id });
 
-            // Fancy way for merging objects together
-            const newTag = Object.assign(tag, { 'name': newName ? newName : tag.name, 'userid': user.id });
+        await db.addData('tags', newTag);
 
-            await db.addData('tags', newTag);
-            await msg.channel.send(`${user} you now have ${newTag.name} tag on your list!`);
+        awaitingUsers.splice(
+            awaitingUsers.indexOf(m.author), 1
+        );
 
+        await msg.channel.send(`You now have \`${newTag.name}\` tag on your list!`);
+
+        if(awaitingUsers.length == 0)
             msgC.stop();
-        });
-
-        msgC.on('end', c => {
-            awaitingUsers.splice(
-                awaitingUsers.indexOf(c.first().author.id), 1
-            );
-        });
-    }
+    });
 
     // Get user strings so we can mention all people who we are sharing tag to
     const allUsers = awaitingUsers.map(u => u.toString());
-    if(allUsers.length > 0)
-        await msg.channel.send(`${allUsers.join(' ')} please confirm if you want to recieve this tag by typing \`${code}\` in the chat!`);
+
+    const shareMsg = await msg.channel.send(`Please confirm if you want to recieve this tag by typing \`${code}\` in the chat!`);
+
+    msgC.on('end', async() => {
+        if(shareMsg && shareMsg.editable)
+            await shareMsg.edit(shareMsg.content.replace(code, 'expired'));
+    });
 }
