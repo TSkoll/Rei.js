746490db5b7f68f2a509b2651a9fc9cd675700f5
nrabulinski <nikodemrabulinski@gmail.com>
2018-05-10 19:14:07 +0200

    Commenting

--- modules/Misc/tag/rename.js
+++ modules/Misc/tag/rename.js
@@ -3,7 +3,10 @@ const db = require('../../../utils/dbUtil.js');
 module.exports = async function(msg, args) {
     if (args.length < 3)
         throw 'Not enough arguments!';
+    if(args[1] === args[2])
+        throw 'I can\'t rename a tag with the same name'; 
 
+    // We try to get quotes which have either the same name as the old name or the new one
     const rows = 
         (await db.table('tags')
         .where({ 'userid': msg.author.id })
@@ -11,14 +14,15 @@ module.exports = async function(msg, args) {
             builder.where({'name': args[1]}).orWhere({ 'name': args[2] })
         }).select('name')).map(row => row.name);
 
-    // If row doesn't exist - throw
+    // And here we check if user has either
+    // If they don't have a tag with the old name that means it doesn't exist
     if (!rows.includes(args[1]))
         throw 'I can\'t rename a tag that doesn\'t exist!';
 
-    // Check if user doesn't have a tag with the same name
+    // However if they have a tag with the new name we can't duplicate names
     if(rows.includes(args[2]))
         throw 'A tag with that name already exists!';
 
-    // Row exists, we can edit it
+    // Here we're sure the user has both old tag and the name won't be repeated
     await db.updateRow('tags', { 'userid': msg.author.id, 'name': args[1] }, { 'name': args[2] });
 }
