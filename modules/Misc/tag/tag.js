82702251a71034f61b415c7881962d032300dbbc
DoubleKilled <ronkainen.eero@gmail.com>
2018-07-09 19:19:56 +0300

    Update tag to use tag name as file name on upload

--- modules/Misc/tag/tag.js
+++ modules/Misc/tag/tag.js
@@ -2,7 +2,8 @@ const db = require('../../../utils/dbUtil.js');
 const tagUtils = require('./tagUtils.js');
 
 module.exports = async function(msg, args) {
-    const rows = await db.getRows('tags', { 'userid': msg.author.id, 'name': args.join(' ') });
+    const tagName = args.join(' ');
+    const rows = await db.getRows('tags', { 'userid': msg.author.id, 'name': tagName });
 
     if (rows.length < 1)
         throw 'This tag doesn\'t seem to exist!';
@@ -17,7 +18,7 @@ module.exports = async function(msg, args) {
         file = await tagUtils.loadImage(rows[0].imageid);
     
     if (file)
-        await msg.channel.send(content, { files: [ { attachment: file, name: rows[0].imageid } ] });
+        await msg.channel.send(content, { files: [ { attachment: file, name: tagName + rows[0].imageid.slice(rows[0].imageid.indexOf('.')) } ] });
     else
         await msg.channel.send(content);
 }
