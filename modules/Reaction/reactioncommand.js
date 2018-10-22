f95b278f475324670d21085610c9b55ac09b821f
DoubleKilled <ronkainen.eero@gmail.com>
2018-10-22 22:11:17 +0300

    Update reaction commands to support multiple mentions

--- modules/Reaction/reactioncommand.js
+++ modules/Reaction/reactioncommand.js
@@ -10,17 +10,39 @@ class ReactionCommand extends Command {
 
     async sendReaction(msg, reactMessage, query) {
         const reaction = this.imgUrls[Math.floor(Math.random() * this.imgUrls.length)];
-        const mentionedUser = super.searchUser(msg, query);
+
+        if (msg.mentions.members && msg.mentions.members.size > 1 && reactMessage) {
+            let mentionUserString = '';
+            const keys = msg.mentions.members.array().slice(0, msg.mentions.members.size - 2);
+            const rest = msg.mentions.members.array().slice(msg.mentions.members.size - 2);
+
+            for (let i = 0; i < keys.length; i++) {
+                const val = keys[i];
+
+                mentionUserString += val.user.username + ', ';
+            }
+
+
+            mentionUserString += `${rest[0].user.username} and ${rest[1].user.username}`;
+
+            await super.sendEmbed(msg, new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setDescription(reactMessage.replace('%target%', mentionUserString)) 
+            .setImage(reaction));
+        } else {
+            const mentionedUser = super.searchUser(msg, query);
         
-        if (mentionedUser && reactMessage)
-            await super.sendEmbed(msg, new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setDescription(reactMessage.replace('%target%', mentionedUser.user.username)) 
-            .setImage(reaction));
-        else
-            await super.sendEmbed(msg, new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setImage(reaction));
+            if (mentionedUser && reactMessage) {
+                await super.sendEmbed(msg, new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setDescription(reactMessage.replace('%target%', mentionedUser.user.username)) 
+                .setImage(reaction));
+            } else {
+                await super.sendEmbed(msg, new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setImage(reaction));
+            }
+        }
     }
 }
 module.exports = ReactionCommand;
