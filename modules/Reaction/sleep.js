0f0789c01748174c7c0dcbc1bc95f5bb6c4e7bd9
DoubleKilled <ronkainen.eero@gmail.com>
2018-11-07 18:00:18 +0200

    Improve sleep

--- modules/Reaction/sleep.js
+++ modules/Reaction/sleep.js
@@ -1,4 +1,5 @@
 const ReactionCommand = require('./reactioncommand');
+const Discord = require('discord.js');
 
 const sleeps = [
     'https://i.imgur.com/jLHk2UK.gif',
@@ -24,6 +25,46 @@ class Sleep extends ReactionCommand {
     }
 
     async run(bot, msg, args) {
+        if (args) {
+            const messages = await msg.channel.fetchMessages({limit: 5});
+            const reiEmbedMessage = messages.find(x => x.author.id == bot.user.id && x.embeds.length > 0);
+    
+            if (reiEmbedMessage) {
+                const embed = reiEmbedMessage.embeds[0];
+                const eDesc = embed.description;
+
+                // Make sure the embed we noticed is actually for sleeping
+                if (eDesc && eDesc.includes('sleep')) {
+                    // Clean description out of stuff we don't want, replace with REGEX later on
+                    const previousUsers = eDesc.slice(0, eDesc.indexOf('put') - 1)
+                                .split('**')
+                                .filter(el => 
+                                    el != "" &&
+                                    el != ", " &&
+                                    el != " and " &&
+                                    el != " put " &&
+                                    el != " to slee"
+                                );
+    
+                    // + 5 due to "put(s) ", - 1 due to " "
+                    const targetUserStart = eDesc.indexOf('put') + 5;
+                    const targetUserEnd = eDesc.indexOf('to sleep!') - 1;
+    
+                    const targetUser = eDesc.slice(targetUserStart, targetUserEnd).replace(/\*/g, '');
+                    const user = super.searchUser(msg, args);
+
+                    if (targetUser === user.user.username) {
+                        await reiEmbedMessage.edit(new Discord.RichEmbed()
+                        .setColor(embed.hexColor)
+                        .setImage(embed.image.url)
+                        .setDescription(`${previousUsers.map(x => `**${x}**`).join(', ')} and **${msg.author.username}** put **${targetUser}** to sleep!`));
+
+                        return;
+                    }
+                }
+            }
+        }
+        
         await super.sendReaction(msg, `**${msg.author.username}** puts **%target%** to sleep!`, args);
     }
 }
